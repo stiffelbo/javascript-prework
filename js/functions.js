fd75c55495128863a9f899668cb4ee27aeaf3ad2
StiffElbo <partygrouppl@gmail.com>
2020-12-07 00:40:57 +0100

    final

--- js/functions.js
+++ js/functions.js
@@ -10,11 +10,11 @@ function clearMessages() {
 
 function getMoveName(argMoveId) {
     if (argMoveId == 1) {
-        return 'kamień';
+        return 'kamien';
     } else if (argMoveId == 2) {
         return 'papier';
     } else if (argMoveId == 3) {
-        return 'nożyce';
+        return 'nozyce';
     } else {
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
         return 'nieznany ruch';
@@ -22,37 +22,51 @@ function getMoveName(argMoveId) {
 }
 
 function displayCounter(argRobotCounter, argPlayerCounter) {
-    console.log(argRobotCounter + "  " + argPlayerCounter);
-    let playerCSS = "background-image: linear-gradient(90deg, red " + argPlayerCounter * 10 + "%, transparent " + argPlayerCounter * 10 + "%);"
-    let robotCSS = "background-image: linear-gradient(90deg, red " + argRobotCounter * 10 + "%, transparent " + argRobotCounter * 10 + "%);"
+    //console.log(argRobotCounter + "  " + argPlayerCounter);
+    let robotCSS = "background-image: linear-gradient(90deg, red " + argPlayerCounter * 10 + "%, transparent " + argPlayerCounter * 10 + "%);"
+    let playerCSS = "background-image: linear-gradient(90deg, red " + argRobotCounter * 10 + "%, transparent " + argRobotCounter * 10 + "%);"
     document.getElementById("playerCounter").style.cssText = playerCSS;
     document.getElementById("robotCounter").style.cssText = robotCSS;
+    document.getElementById("round-count").innerText = "Round: " + roundCounter;
 
-    printMessage("robot: " + argRobotCounter);
-    printMessage("player: " + argPlayerCounter);
+    if (argRobotCounter == maxCounter) {
+        printMessage('<h1>You Loose!</h1>');
+        printMessage('<h5>Pick to play again</h5>');
+        robotCounter = playerCounter = roundCounter = 0;
+    } else if (argPlayerCounter == maxCounter) {
+        printMessage('<h1>You Win!</h1>');
+        printMessage('<h5>Pick to play again</h5>');
+        robotCounter = playerCounter = roundCounter = 0;
+    }
 }
 
 function displayResult(argComputerMove, argPlayerMove) {
 
-    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    // printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-    if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-        printMessage('Ty wygrywasz!');
+    if (argComputerMove == 'kamien' && argPlayerMove == 'papier') {
+        // printMessage('Ty wygrywasz!');
+        document.getElementById('papier').className += ' win';
         playerCounter++;
         displayCounter(robotCounter, playerCounter);
-    } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-        printMessage('Ty wygrywasz!');
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'nozyce') {
+        // printMessage('Ty wygrywasz!');
+        document.getElementById('nozyce').className += ' win';
         playerCounter++;
         displayCounter(robotCounter, playerCounter);
-    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-        printMessage('Ty wygrywasz!');
+    } else if (argComputerMove == 'nozyce' && argPlayerMove == 'kamien') {
+        // printMessage('Ty wygrywasz!');
+        document.getElementById('kamien').className += ' win';
         playerCounter++;
         displayCounter(robotCounter, playerCounter);
     } else if (argComputerMove == argPlayerMove) {
-        printMessage('Remis');
+        //printMessage('Remis');
+        document.getElementById(argComputerMove + "-robot").className += ' draw';
+        document.getElementById(argPlayerMove).className += ' draw';
         displayCounter(robotCounter, playerCounter);
     } else {
-        printMessage('Tym razem przegrywasz :(');
+        // printMessage('Tym razem przegrywasz :(');
+        document.getElementById(argComputerMove + "-robot").className += ' win';
         robotCounter++;
         displayCounter(robotCounter, playerCounter);
     }
--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
+var roundCounter = 1;
+var robotCounter = 0;
+var playerCounter = 0;
+var maxCounter = 10;
+
+
 document.getElementById("kamien").addEventListener('click', function() { playGame(1); });
 document.getElementById("papier").addEventListener('click', function() { playGame(2); });
 document.getElementById("nozyce").addEventListener('click', function() { playGame(3); });
 
-let roundCounter = 0;
-let robotCounter = 0;
-let playerCounter = 0;
-let maxCounter = 10;
-
 function playGame(playerInput) {
     clearMessages();
+    roundCounter++;
+    //clear previous robot choice
+    let robotBtns = document.querySelectorAll(".robot-btn");
 
+    for (let i = 0; i < robotBtns.length; i++) {
+        robotBtns[i].className = "robot-btn";
+    }
+    //clear previous player choice
+    let playerBtns = document.querySelectorAll(".player-btn");
+
+    for (let i = 0; i < playerBtns.length; i++) {
+        playerBtns[i].className = "player-btn";
+    }
+
+    //robot move
     let randomNumber = Math.floor(Math.random() * 3 + 1);
 
     let computerMove = getMoveName(randomNumber);
-
+    console.log(computerMove);
+    document.getElementById(computerMove + "-robot").className += " picked";
+    //player move
     let playerMove = getMoveName(playerInput);
-
+    document.getElementById(playerMove).className += " picked";
     displayResult(computerMove, playerMove);
 
 }
