93e7781dd2e4b487757623323a10d995fca47a59
StiffElbo <partygrouppl@gmail.com>
2020-12-06 17:17:11 +0100

    basic game logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,48 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+// computer move 
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
+
+let computerMove = Math.floor(Math.random() * 3 + 1);
+
+if (computerMove == 1) {
+    computerMove = 'kamień';
+} else if (computerMove == 2) {
+    computerMove = 'papier';
+} else if (computerMove == 3) {
+    computerMove = 'nożyce';
+}
+
+console.log('Komputer zagrał : ' + computerMove);
+
+
+// player move 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+}
+
+//getting result
+
+let result = 'Komputer wygrywa!';
+
+if (playerMove == 'nieznany ruch') {
+    result = 'Podałeś ' + playerMove + ', przegrywasz ;-P';
+} else if (computerMove == 'kamień' && playerMove == 'papier') {
+    result = 'Wygrywasz!';
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    result = 'Wygrywasz!';
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    result = 'Wygrywasz!';
+} else if (computerMove == playerMove) {
+    result = 'Remis!';
+}
+
+printMessage('Twój ruch to: ' + playerMove + '<br> Ruch komputera: ' + computerMove + '<br> Wynik rundy: ' + result);
